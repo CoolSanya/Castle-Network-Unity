3351039492b64c9fc9f2b1e23a15fb5afa2de1ec
CoolSanya <Zhyhy123@gmail.com>
2022-06-03 16:24:22 +0300

    init

---
+++ serverPV915/ServerMD/Controllers/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServerMD.Models;
+
+namespace ServerMD.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class GameController : ControllerBase
+    {
+        private static List<Solider> list=new List<Solider>();
+        //Отримуємо дані про постріл
+        [HttpGet("{nick}")]
+        public IActionResult Index(string nick)
+        {
+            var solider = list.SingleOrDefault(s => s.Nick == nick);
+            if(solider!=null)
+                list.Remove(solider);
+            return Ok(solider);
+        }
+        //Стриляємо по обєкту
+        [HttpPost]
+        public IActionResult Post([FromBody]Solider solider)
+        {
+            if(solider!=null)
+            {
+                Console.WriteLine("Begin game "+solider.Nick);
+                list.Add(solider);
+            }
+            return Ok();
+        }
+
+    }
+}
